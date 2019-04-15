2dc29e4716f74ce1ebf1f7504562b1a9af35a94d
Niboucha Redouane <rniboucha@yahoo.fr>
2019-04-15 21:48:27 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,26 +1,22 @@
 import anonymizer
+from time import time
 
 # test GET
 
-print(  anonymizer.get(url='https://httpbin.org/get?arg1=123&arg2=456') )
+print(  anonymizer.get(url='https://httpbin.org/get?arg1=123&arg2=456').text )
 
 # test POST
 
-print(  anonymizer.post(url='https://httpbin.org/post', data={'arg1':'123','arg2':'456'}) )
+print(  anonymizer.post(url='https://httpbin.org/post', data={'arg1':'123','arg2':'456'}).text )
 
-# try checking IP between successive requests
-
-for i in range(3):
+# try checking IP between 6 successive requests
+t = time()
+for i in range(6):
   print(anonymizer.get_ip())
-
-# now, change the IP manually between the requests
-
-for i in range(3):
-  print(anonymizer.get_ip())
-  anonymizer.new_identity()
-
+print("Time taken : " + str(time() - t))
 # try simultaneous requests
 
 obj = { 'method': 'GET', 'url': "https://icanhazip.com/" }
-
+t = time()
 print([x.text for x in simultaneous_requests( [ obj, obj, obj, obj, obj, obj ] )])
+print("time taken in simultaneous_requests : " + str(time() - t))
