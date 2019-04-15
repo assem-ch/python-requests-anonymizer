1aaa2de3fd9ae5ea97bf5ddef9f63295fad2291d
Niboucha Redouane <rniboucha@yahoo.fr>
2019-04-15 22:06:09 +0100

    Update anonymizer.py

--- anonymizer.py
+++ anonymizer.py
@@ -4,12 +4,12 @@ TIMEOUT = 5
 def get(**args):
 	args['proxies'] = {'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
 	args['timeout'] = TIMEOUT
-	return grequests.map([grequests.get(**args)])
+	return grequests.map([grequests.get(**args)])[0]
 
 def post(**args):
 	args['proxies'] = {'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
 	args['timeout'] = TIMEOUT
-	return grequests.map([grequests.post(**args)])
+	return grequests.map([grequests.post(**args)])[0]
 
 def simultaneous_requests(lst):
 	# lst is an array of dicts, each dict has the key 'method' that is 'get', 'post', or any other HTTP method, url, and other parameters we might want to pass to grequests.request
