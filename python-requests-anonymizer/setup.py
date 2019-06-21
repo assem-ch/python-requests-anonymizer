d24043633e5f41229acccfe0847152b76698e003
Assem Chelli <assem.ch@gmail.com>
2019-06-21 01:48:04 +0100

    fix dependencies

--- python-requests-anonymizer/setup.py
+++ python-requests-anonymizer/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='requests_anonymizer',
-    version='0.1',
+    version='0.2',
     packages=['.'],
-
     url='https://github.com/assem-ch/python-requests-anonymizer',
+    install_requires=['grequests'],
     license='MIT',
     author='Assem Chelli; Niboucha Redouane',
     author_email='',
